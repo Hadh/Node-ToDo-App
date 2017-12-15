64b8176cc93410ce6b00d1e7717c13f707e04dcf
Hadhemi <soccerstar.laouini@gmail.com>
2017-12-15 23:11:37 +0100

    Demo

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -5,21 +5,21 @@ var Todo = require('../models/Todo')
 
 module.exports = function(app){
     
-    app.get('/todo',function(req, res){
+    app.get('/',function(req, res){
         Todo.find({},function(err, data){
             if(err) throw err;
             res.render('todo',{todos:data})
         })
     });
 
-    app.post('/todo', urlencodedParser, function(req, res){
+    app.post('/', urlencodedParser, function(req, res){
         var newTodo = Todo(req.body).save(function(err,data){
             if(err) throw err;
             res.json(data);
         });
     });
 
-    app.delete('/todo/:item',function(req, res){
+    app.delete('/:item',function(req, res){
         // because items with spaces comes with - in the URL.
         Todo.find({item: req.params.item.replace(/\-/g," ")}).remove(function(err,data){
             if(err) throw err;
