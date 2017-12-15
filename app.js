0ca4ae0b99e72fdfaba2c3aab498f6b51e2bba62
Hadhemi <soccerstar.laouini@gmail.com>
2017-12-15 23:18:26 +0100

    port change

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express')
 var app = express();
 var mongoose = require('mongoose');
 
+app.set('port', (process.env.PORT || 3000));
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
 mongoose.connect('mongodb://hdmi:hdmi@ds141766.mlab.com:41766/todo');
@@ -9,6 +10,6 @@ mongoose.connect('mongodb://hdmi:hdmi@ds141766.mlab.com:41766/todo');
 var todoController = require('./controllers/todoController');
 todoController(app);
 
-
-app.listen(3000)
-console.log('Listening on Port 3000')
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+  });
